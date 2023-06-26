53df25e9df7edd5320481da33b84e9f11145cf3b
Steven Snoeijen <stevensnoeijen@gmail.com>
2023-06-26 23:24:07 +0200

    chore: set output in action

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ set -eo pipefail
 
 UNUSED_JSON=$(npx --yes depcheck@1.4.3 --ignores="${INPUT_IGNORES}" test/ --json)
 
-echo "UNUSED_JSON=test" >> "$GITHUB_OUTPUT"
+echo "unused_json=test" >> "$GITHUB_OUTPUT"
 echo "exitcode=0" >> "$GITHUB_OUTPUT"
