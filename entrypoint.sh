4902dbfc61e8cbff744d2bd12ccf53ec32569255
Steven Snoeijen <stevensnoeijen@gmail.com>
2023-06-26 23:20:13 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ set -eo pipefail
 
 UNUSED_JSON=$(npx --yes depcheck@1.4.3 --ignores="${INPUT_IGNORES}" test/ --json)
 
-echo "UNUSED_JSON=$UNUSED_JSON" >> "$GITHUB_OUTPUT"
+echo "UNUSED_JSON=test" >> "$GITHUB_OUTPUT"
 echo "exitcode=0" >> "$GITHUB_OUTPUT"
