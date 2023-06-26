67f0cf6f883cbfd936c20268a9a4150eaeedde3f
Steven Snoeijen <stevensnoeijen@gmail.com>
2023-06-26 23:34:15 +0200

    fix exit_code

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/env bash
 
-set -eo pipefail
-
-UNUSED_JSON=$(npx --yes depcheck@1.4.3 --ignores="${INPUT_IGNORES}" test/ --json)
-
-echo "unused_json=test" >> "$GITHUB_OUTPUT"
-echo "exitcode=0" >> "$GITHUB_OUTPUT"
-
-exit 0
+echo "unused_json=$(npx --yes depcheck@"${INPUT_VERSION}" --ignores="${INPUT_IGNORES}" "${INPUT_DIR}" --json)" >> "$GITHUB_OUTPUT"
