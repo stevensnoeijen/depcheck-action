1037f5319baf8ae17853b718d6399410d065fff3
Steven Snoeijen <stevensnoeijen@gmail.com>
2023-06-26 23:09:51 +0200

    fix: entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,6 @@
 
 set -eo pipefail
 
-UNUSED_JSON=$(npx depcheck@"${INPUT_VERSION}" --ignores="${INPUT_IGNORES}" "${INPUT_DIR}" --json 2>&1 && exit_status=$? || exit_status=$?)
+UNUSED_JSON=$(npx --yes depcheck@"${INPUT_VERSION}" --ignores="${INPUT_IGNORES}" "${INPUT_DIR}" --json)
 
 echo "UNUSED_JSON=$UNUSED_JSON" >> "$GITHUB_OUTPUT"
-echo "exit_code=$exit_status" >> "$GITHUB_OUTPUT"
