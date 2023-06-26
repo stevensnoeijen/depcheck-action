7b5bd76ce488273466e8018c683b88162383f5e7
Steven Snoeijen <stevensnoeijen@gmail.com>
2023-06-26 23:02:14 +0200

    fix: log unused dependencies

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 
 set -eo pipefail
 
-npx depcheck@"${INPUT_VERSION}" --ignores="${INPUT_IGNORES}" "${INPUT_DIR}" --json 2>&1 && exit_status=$? || exit_status=$?
+UNUSED_JSON=$(npx depcheck@"${INPUT_VERSION}" --ignores="${INPUT_IGNORES}" "${INPUT_DIR}" --json)
 
-echo "exit_code=$exit_status" >> "$GITHUB_OUTPUT"
+echo "UNUSED_JSON=$UNUSED_JSON" >> "$GITHUB_OUTPUT"
