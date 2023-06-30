b8c1a2ebac61f010f06be2112fe95f68ef2b2b00
Steven Snoeijen <stevensnoeijen@gmail.com>
2023-06-30 22:47:53 +0200

    fix: escape and unescape json to be able to process it

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-unused_json=$(npx --yes depcheck@"${INPUT_VERSION}" --ignores="${INPUT_IGNORES}" "${INPUT_DIR}" --json)
+unused_json=$(npx --yes depcheck@"${INPUT_VERSION}" --ignores="${INPUT_IGNORES}" "${INPUT_DIR}" --json | jq -Rsa .)
 
 echo "unused_json=$unused_json" >> "$GITHUB_OUTPUT"
