35187af9befe56d6ca908c8e51a3c7fec880824d
Steven Snoeijen <stevensnoeijen@gmail.com>
2023-07-02 21:15:59 +0200

    fix: depcheck entrypoint to remove newline symbol

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-unused_json=$(npx --yes depcheck@"${INPUT_VERSION}" --ignores="${INPUT_IGNORES}" "${INPUT_DIR}" --json | jq -Rsa .)
+unused_json=$(npx --yes depcheck@"${INPUT_VERSION}" --ignores="${INPUT_IGNORES}" "${INPUT_DIR}" --json | jq -Rsa . | sed 's/\\[tn]//g')
 
 echo "unused_json=$unused_json" >> "$GITHUB_OUTPUT"
