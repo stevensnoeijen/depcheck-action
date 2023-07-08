28f9d33283abfc01ee0bf8f246ed366c9bec1aaa
Steven Snoeijen <stevensnoeijen@gmail.com>
2023-07-08 16:13:16 +0200

    ref: simplify entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-unused_json=$(npx --yes depcheck@"${INPUT_VERSION}" --ignores="${INPUT_IGNORES}" "${INPUT_DIR}" --json | jq -Rsa . | sed 's/\\[tn]//g')
-
-echo "unused_json=$unused_json" >> "$GITHUB_OUTPUT"
+echo "unused_json=$(npx --yes depcheck@"${INPUT_VERSION}" --ignores="${INPUT_IGNORES}" "${INPUT_DIR}" --json | jq -Rsa . | sed 's/\\[tn]//g')" >> "$GITHUB_OUTPUT"
