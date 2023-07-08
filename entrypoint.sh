d5d9c9bf8e5140c23d2d77340b842122c4a6ccce
Steven Snoeijen <stevensnoeijen@gmail.com>
2023-07-08 17:07:13 +0200

    ref: move depcheck script t o action

--- entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-echo "unused_json=$(npx --yes depcheck@"${INPUT_VERSION}" --ignores="${INPUT_IGNORES}" "${INPUT_DIR}" --json | jq -Rsa . | sed 's/\\[tn]//g')" >> "$GITHUB_OUTPUT"
